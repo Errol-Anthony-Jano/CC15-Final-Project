70c987e7f706bd9b71bd2476f5a156079b61d284
eggroll-tek <jano.errol22@gmail.com>
2025-05-03 02:05:06 +0800

    Deleted signup.py, deleted wrapper_signup.py

--- wrapper_signup.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QWidget
-from signup import Ui_SignupWindow
-
-class SignupWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.ui = Ui_SignupWindow()
-        self.ui.setupUi(self)
