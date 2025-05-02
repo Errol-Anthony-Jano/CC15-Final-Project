8630b41d8d5b9f02a3f3bff83b0035306b779793
eggroll-tek <jano.errol22@gmail.com>
2025-05-03 01:57:10 +0800

    Updated primary-window.py to link registration section to login section

--- primary-window.py
+++ primary-window.py
@@ -9,20 +9,26 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(self.stack)
 
         self.login = uic.loadUi(r"resources\ui-files\login.ui")
+        self.signup = uic.loadUi(r"resources\ui-files\sign-up.ui")
         self.dashboard = uic.loadUi(r"resources\ui-files\bank-dashboard.ui")
 
         self.stack.addWidget(self.login)
+        self.stack.addWidget(self.signup)
         self.stack.addWidget(self.dashboard)
 
-        self.login.pushButton.clicked.connect(self.showDashboard)
+        self.login.btn_login.clicked.connect(self.showDashboard)
         self.dashboard.btn_logout.clicked.connect(self.showLogin)
+        self.login.register_button.clicked.connect(self.showRegistration)
 
     
     def showDashboard(self):
-        self.stack.setCurrentIndex(1)
+        self.stack.setCurrentIndex(2)
 
     def showLogin(self):
         self.stack.setCurrentIndex(0)
+
+    def showRegistration(self):
+        self.stack.setCurrentIndex(1)
 
 app = QApplication([])
 window = MainWindow()
