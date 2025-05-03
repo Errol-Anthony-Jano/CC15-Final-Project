9bf407b49ce0cbe2460550008b9d51240af46e79
eggroll-tek <jano.errol22@gmail.com>
2025-05-03 16:58:41 +0800

    Modified setup of the window

--- primary-window.py
+++ primary-window.py
@@ -1,35 +1,59 @@
 from PyQt6 import uic
-from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QStackedWidget, QHBoxLayout, QSizePolicy
 
 class MainWindow(QMainWindow):
     def __init__(self):
+        # Initialize window, window name, and layout to be used.
         super().__init__()
+        self.setWindowTitle("CC15 Banking System")
+        self.setStyleSheet("background: #ffffff")
 
+        #Initialize HBoxLayout
+        self.layout = QHBoxLayout()
+
+        #Initialize central widget
+        self.central_widget = QWidget()
+
+        #Initialize StackedWidget
         self.stack = QStackedWidget()
-        self.setCentralWidget(self.stack)
+        self.stack.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
 
-        self.login = uic.loadUi(r"resources\ui-files\login.ui")
-        self.signup = uic.loadUi(r"resources\ui-files\signup-resized.ui")
+        #Initialize NavBar 
+        self.navbar = uic.loadUi(r"resources\ui-files\navbar.ui")
+        self.navbar.setSizePolicy(QSizePolicy.Policy.Preferred, QSizePolicy.Policy.Expanding)
+
+        #Add elements to layout
+        self.layout.addWidget(self.navbar)
+        self.layout.addWidget(self.stack)
+
+        self.layout.setStretch(0, 1)
+        self.layout.setStretch(1, 4)
+
+        #Add elements to stack
         self.dashboard = uic.loadUi(r"resources\ui-files\bank-dashboard.ui")
+        self.transferPanel = uic.loadUi(r"resources\ui-files\transfer-panel.ui")
 
-        self.stack.addWidget(self.login)
-        self.stack.addWidget(self.signup)
         self.stack.addWidget(self.dashboard)
+        self.stack.addWidget(self.transferPanel)
 
-        self.login.btn_login.clicked.connect(self.showDashboard)
-        self.dashboard.btn_logout.clicked.connect(self.showLogin)
-        self.login.register_button.clicked.connect(self.showRegistration)
-        self.signup.pushButton.clicked.connect(self.showLogin)
+        #Set central widget
+        self.central_widget.setLayout(self.layout)
+        self.setCentralWidget(self.central_widget)
 
-    
+
+        self.navbar.btn_transfer.clicked.connect(self.showTransferPanel)
+        self.navbar.btn_dashboard.clicked.connect(self.showDashboard)
+
+    def showTransferPanel(self):
+        self.stack.setCurrentIndex(1)
+
     def showDashboard(self):
-        self.stack.setCurrentIndex(2)
-
-    def showLogin(self):
         self.stack.setCurrentIndex(0)
 
-    def showRegistration(self):
-        self.stack.setCurrentIndex(1)
+        
+
+
+        
 
 app = QApplication([])
 window = MainWindow()
