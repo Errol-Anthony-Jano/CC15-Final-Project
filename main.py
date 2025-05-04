48b59e32add2e4757eef8e1a6d9f34719916a108
Errol-Anthony-Jano <jano.errol2005@gmail.com>
2025-05-05 01:54:31 +0800

    Added main.py as the driver code of the project

---
+++ main.py
@@ -0,0 +1,43 @@
+from PyQt6 import uic
+from PyQt6.QtWidgets import QApplication, QMainWindow, QStackedWidget
+
+from views.dashboard_view import DashboardUI
+from controllers.login_control import LoginAppControl
+from controllers.dashboard_control import DashboardAppControl
+from controllers.register_control import RegisterAppControl
+
+
+
+import sys
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("CC15 Banking System")
+        self.setStyleSheet("background: #ffffff")
+
+        # initialize qstackedwidget for switching between login/register and main menu
+        self.central_widget = QStackedWidget()
+
+        # set central_widget as central widget
+        self.setCentralWidget(self.central_widget)
+
+        self.loginPanel = uic.loadUi(r"resources\ui-files\login.ui");
+        self.registerPanel = uic.loadUi(r"resources\ui-files\signup_resized.ui")
+        self.dashboard = DashboardUI()
+
+        self.central_widget.addWidget(self.loginPanel) #index 0
+        self.central_widget.addWidget(self.registerPanel) # index 1 
+        self.central_widget.addWidget(self.dashboard) # index 2
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+
+login_control = LoginAppControl(window)
+dashboard_control = DashboardAppControl(window)
+register_control = RegisterAppControl(window)
+
+
+window.show()
+app.exec()
