f5d856a6ad3306285666ca31db03ac5d5746973a
Errol-Anthony-Jano <jano.errol2005@gmail.com>
2025-05-07 00:27:55 +0800

    Added more fields to UserSession class

--- models/user_session.py
+++ models/user_session.py
@@ -1,4 +1,9 @@
 class UserSession:
     def __init__(self):
         self.user_id = None
+        self.first_name = None
+        self.last_name = None
         self.username = None
+        self.account_number = None
+        self.password = None
+        self.balance = None
