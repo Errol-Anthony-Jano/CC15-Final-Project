d780cd603b78c9db867b08c9b8a46026d98fa0c4
Errol-Anthony-Jano <jano.errol2005@gmail.com>
2025-05-11 23:28:20 +0800

    Added extra fields for easy retrieval of user_id/account_number and other attributes

--- models/user_session.py
+++ models/user_session.py
@@ -6,4 +6,48 @@ class UserSession:
         self.username = None
         self.account_number = None
         self.password = None
-        self.balance = None
+        self.balance = None # stored in cents, displayed in pesos
+
+    def get_user_id(self):
+        return self.user_id
+
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.last_name
+
+    def get_username(self):
+        return self.username
+
+    def get_account_number(self):
+        return self.account_number
+
+    def get_password(self):
+        return self.password
+
+    def get_balance(self):
+        return self.balance
+
+    # SETTERS
+
+    def set_user_id(self, user_id):
+        self.user_id = user_id
+    
+    def set_first_name(self, first_name):
+        self.first_name = first_name
+    
+    def set_last_name(self, last_name):
+        self.last_name = last_name
+
+    def set_username(self, username):
+        self.username = username
+
+    def set_account_number(self, account_number):
+        self.account_number = account_number
+
+    def set_password(self, password):
+        self.password = password
+
+    def set_balance(self, balance):
+        self.balance = balance
